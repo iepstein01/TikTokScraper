26ed5210989645428b6003cddb58250fe4116266
itaie <itainepstein@gmail.com>
2023-11-29 15:38:10 -0500

    refactoring scraper, testing in notebook

--- Archive/user_interests.py
+++
@@ -1,9 +0,0 @@
-def get_interests():
-    interests = []
-    while True:
-        interest = input(
-            'What are you interests? Please use single words, type "done" when finished.'
-        )
-        if interest == "done".lower():
-            return interests
-        interests.append(interest)
